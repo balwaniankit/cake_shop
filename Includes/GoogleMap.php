2f65e54725e8eaf72c2e2efb2d5525b62183f5cc
ankitbalwani <2020.ankit.balwani@ves.ac.in>
2022-10-30 13:55:19 +0530

    first commit

---
+++ Includes/GoogleMap.php
@@ -0,0 +1,12 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>LOCATE US</h2>
+    </div>
+
+    <div class="google-map">
+        <iframe src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Velvette Patisserie&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%"" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+</div>
+
