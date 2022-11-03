8097d7b45d2a7eec3ad9a39902a045888ad4ac05
ankitbalwani <2020.ankit.balwani@ves.ac.in>
2022-11-03 23:43:05 +0530

    second commit

--- registration.php
+++ registration.php
@@ -156,7 +156,7 @@
                     $headers = "From: malako.cakeshop@gmail.com \r\n";
                     $headers .= "MIME-Version: 1.0" . "\r\n";
                     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
+									
                     mail($to, $subject, $message, $headers);
                     setcookie("thankYouCookie", "verificationEmailSent");
                     header('location: thankYouRegistration.php');
